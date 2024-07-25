833f9c445b1924b8b65b92a369c6f18020c3d953
kikemocha <enriquemorarey@gmail.com>
2024-07-25 13:43:59 +0200

    version 1.1

--- LAB.sql
+++ LAB.sql
@@ -1,20 +1,23 @@
 use sakila;
 
-CREATE VIEW customer_summary_report AS
-SELECT r.customer_id, c.first_name, c.email,count(*) AS rental_count FROM rental AS r
-JOIN customer AS c
-ON c.customer_id = r.customer_id
-GROUP BY r.customer_id;
+CREATE VIEW customer_rental_summary AS  
+SELECT c.customer_id,CONCAT(c.first_name, ' ', c.last_name) AS customer_name, c.email, COUNT(*) AS rental_count 
+FROM customer AS c
+JOIN rental AS r ON c.customer_id = r.customer_id
+GROUP BY c.customer_id
+ORDER BY rental_count DESC;
 
-CREATE TEMPORARY TABLE sakila.temp_customer_table
-SELECT cs.customer_id, SUM(amount) AS total_amount FROM customer_summary_report AS cs
-JOIN payment AS p
-ON p.customer_id = cs.customer_id
-GROUP BY customer_id;
-
+CREATE TEMPORARY TABLE customer_payment_summary AS (
+    SELECT crs.customer_id, SUM(p.amount) AS total_paid
+    FROM customer_rental_summary AS crs
+    JOIN rental AS r ON crs.customer_id = r.customer_id
+    JOIN payment AS p ON r.rental_id = p.rental_id
+    GROUP BY crs.customer_id
+);
 
 WITH cte_customer_table AS(
-    SELECT csr.*,tc.total_amount AS total_paid FROM sakila.customer_summary_report AS csr
+    SELECT csr.*,tc.total_amount AS total_paid, (tc.total_amount / csr.rental_count) AS average_payment_per_rental
+    FROM sakila.customer_summary_report AS csr
     JOIN sakila.temp_customer_table AS tc
     ON csr.customer_id = tc.customer_id
 )
